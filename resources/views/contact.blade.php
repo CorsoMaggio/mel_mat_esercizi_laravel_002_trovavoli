3fc9dac9f4fb9d466808189ea8df31c7c88ca0f8
Melaniamat <melaniamatacena.97@gmail.com>
2025-06-18 11:43:57 +0200

    add-validation -for

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,15 @@
         Form Contatti
     </x-slot>
     <h1>Contattaci!</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action="/invia-email" method="post">
         @csrf
         <div class="mb-3">
